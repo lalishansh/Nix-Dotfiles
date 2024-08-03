61361a9a077f8f99051ab0a9a245204499da88d2
Ishansh Lal <lalishansh@gmail.com>
2024-08-03 22:39:40 +0530

    123 current  2024-08-03 22:39:36  24.11.20240720.2d65995  6.6.41                          *

--- users/ilal/default.nix
+++ users/ilal/default.nix
@@ -6,33 +6,45 @@ let
 in
 {
   # Define the user account.
-  users.users.${username} = {
-    description = "${fullname}";
-    isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" "flatpak" "login" "adbusers" "audio" ]; # `login` group for gnome-keyring
-    initialPassword = username;
+  users.users = let
+    config = {
+      isNormalUser = true;
+      extraGroups = [ "networkmanager" "wheel" "flatpak" "login" "adbusers" "audio" "video" "libvirtd" ]; # `login` group for gnome-keyring
+    };
+  in {
+    ${username} = {
+      inherit (config) isNormalUser extraGroups;
+      description = "${fullname}";
+      initialPassword = username;
+    };
   };
 
-  home-manager.users.${username} = {
-    home.username = username;
-    home.homeDirectory = "/home/${username}";
+  home-manager.users = let
+    config = { user, includes ? [] }:{
+      home.username = user;
+      home.homeDirectory = "/home/${username}";
+      imports = [
+        ./programs
+        (import ./desktop/gitandssh.nix {
+          inherit pkgs;
+          sshProvider = "gnome-keyring";
+          email = emailid;
+          name = fullname;
+        })
+      ] ++ includes;
 
-    imports = [
-      ./desktop
-      ./programs
+      # Config to target compatibility of HomeManager version
+      home.stateVersion = "24.05";
 
-      (import ../common/gitandssh.nix {
-        inherit pkgs;
-        sshProvider = "gnome-keyring";
-        email = emailid;
-        name = fullname;
-      })
-    ];
-
-    # Config to target compatibility of HomeManager version
-    home.stateVersion = "24.05";
-
-    # Let HomeManager install and manage itself
-    programs.home-manager.enable = true;
+      # Let HomeManager install and manage itself
+      programs.home-manager.enable = true;
+    };
+  in {
+    ${username} = config {
+      user = "${username}";
+      includes = [
+        ./desktop/dwl-rofi
+      ];
+    };
   };
 }
